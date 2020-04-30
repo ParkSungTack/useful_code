7e47b4a67675b9d294f06d29d8ba8dd466677d0d
박성택 <56757197+ParkSungTack@users.noreply.github.com>
2020-04-30 15:43:29 +0900

    Update Ab_Pair.cpp

--- Ab_Pair.cpp
+++ Ab_Pair.cpp
@@ -1,10 +1,10 @@
 #include<utility>
 using namespace std;
 
-pair<[type1],[type2]> p;    //정의
+pair<type1,type2> p;    //정의
 
-p.first();                  //p의 첫번째 인자
-p.second();                 //p의 두번째 인자
+p.first;                  //p의 첫번째 인자
+p.second;                 //p의 두번째 인자
 make_pair(num1,num2);       //num1,num2가 들어간 pair생성;
 operator ==,!=,<,>,<=,>=    //정의가 이미 되어있음
 
