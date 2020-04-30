d0975b75715d6e6a692c94779043d02024aa0224
박성택 <56757197+ParkSungTack@users.noreply.github.com>
2020-04-30 16:09:15 +0900

    Update Ab_Pair.cpp

--- Ab_Pair.cpp
+++ Ab_Pair.cpp
@@ -12,3 +12,38 @@ operator ==,!=,<,>,<=,>=    //정의가 이미 되어있음
 
 pair<int,int> p1=make_pair(10,20);
 pair<int,string> p2=make_pair(10,"Hello_World");
+
+//딕셔너리
+#include<map>
+map<string,int> M;
+
+//insert
+m.insert(pair<string,int("string",5)>);
+m.insert(make_pair("string",5));
+m["hello"]=5;
+
+//iterate
+   map<string, int>::iterator it; // auto it = m.begin()도 가능
+    for (it = m.begin(); it != m.end(); it++) {
+        cout << it->first << " " << it->second << endl;
+    }
+    for (pair<string, int> atom : m) {
+        cout << atom.first << " " << atom.second << endl;
+    }
+
+//find
+    m.find("string")->first
+    m.find("string")->second
+    m["string"]
+
+//erase
+    m.erase("string");
+    m.erase(m.find("string"));
+    for (pair<string, int> atom : m) {
+        cout << atom.first << " " << atom.second << endl;
+      
+//size
+    m.size()    
+    
+//empty
+    m.empty()
